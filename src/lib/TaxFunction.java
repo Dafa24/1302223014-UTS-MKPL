fb40d71f44d31629607896a380ef26b454a2f5e5
Dafa24 <akbarpradipa@gmail.com>
2025-04-18 21:40:44 +0700

    Fixing magic number on lines 5 to lines 9

--- src/lib/TaxFunction.java
+++ src/lib/TaxFunction.java
@@ -2,19 +2,12 @@ package lib;
 
 public class TaxFunction {
 
-	
-	/**
-	 * Fungsi untuk menghitung jumlah pajak penghasilan pegawai yang harus dibayarkan setahun.
-	 * 
-	 * Pajak dihitung sebagai 5% dari penghasilan bersih tahunan (gaji dan pemasukan bulanan lainnya dikalikan jumlah bulan bekerja dikurangi pemotongan) dikurangi penghasilan tidak kena pajak.
-	 * 
-	 * Jika pegawai belum menikah dan belum punya anak maka penghasilan tidak kena pajaknya adalah Rp 54.000.000.
-	 * Jika pegawai sudah menikah maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000.
-	 * Jika pegawai sudah memiliki anak maka penghasilan tidak kena pajaknya ditambah sebesar Rp 4.500.000 per anak sampai anak ketiga.
-	 * 
-	 */
-	
-	
+	private static final int BASIC_NONTAXABLE_INCOME = 54000000;
+	private static final int MARRIED_ADDITION = 4500000;
+	private static final int CHILD_ADDITION = 1500000;
+	private static final int MAX_CHILDREN = 3;
+	private static final double TAX_RATE = 0.05;
+
 	public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
 		
 		int tax = 0;
